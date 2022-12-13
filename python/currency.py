f50837c1d5ca75711106df9cdd49933d52d1d4dd
JamesVT103 <118754386+JamesVT103@users.noreply.github.com>
2022-12-13 12:06:22 +0700

    Add files via upload

---
+++ python/currency.py
@@ -0,0 +1,38 @@
+CURRENCY = [
+    'THB', 'AED', 'AFN', 'ALL', 'AMD', 
+    'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 
+    'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 
+    'BHD', 'BIF', 'BMD', 'BND', 'BOB', 
+    'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 
+    'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 
+    'CNY', 'COP', 'CRC', 'CUP', 'CVE', 
+    'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 
+    'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 
+    'FKP', 'FOK', 'GBP', 'GEL', 'GGP', 
+    'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 
+    'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 
+    'HUF', 'IDR', 'ILS', 'IMP', 'INR', 
+    'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 
+    'JOD', 'JPY', 'KES', 'KGS', 'KHR', 
+    'KID', 'KMF', 'KRW', 'KWD', 'KYD', 
+    'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 
+    'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 
+    'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 
+    'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 
+    'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 
+    'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 
+    'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 
+    'QAR', 'RON', 'RSD', 'RUB', 'RWF', 
+    'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 
+    'SGD', 'SHP', 'SLE', 'SLL', 'SOS', 
+    'SRD', 'SSP', 'STN', 'SYP', 'SZL', 
+    'TJS', 'TMT', 'TND', 'TOP', 'TRY', 
+    'TTD', 'TVD', 'TWD', 'TZS', 'UAH', 
+    'UGX', 'USD', 'UYU', 'UZS', 'VES', 
+    'VND', 'VUV', 'WST', 'XAF', 'XCD', 
+    'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 
+    'ZMW', 'ZWL'
+]
+
+API = "https://v6.exchangerate-api.com/v6/a0693466a7ee39b4119ce4e8/latest/THB"
+API = "https://v6.exchangerate-api.com/v6/a0693466a7ee39b4119ce4e8/latest/THB"
